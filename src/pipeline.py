f3578e337b769314808e1efbbb6727e4117f0695
ttmaciag <tomasz.t.maciag@gmail.com>
2020-08-31 21:19:30 +0200

    formatting fixes

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,8 +1,15 @@
+from preprocess import drop_features
 from build_features import prepare_features
 from train import train_model
 from predict import make_prediction
+from pathlib import Path
 
+'''Script for running the whole ML pipeline
+'''
+
+# Parameters
 standardize = True
+
 # Data preprocessing
 prepare_features('data/train.csv', 'data/train_processed.csv', True)
 prepare_features('data/val.csv', 'data/test_processed.csv', True) # NOTE: we rename val to test as it makes more sense
--- src/predict.py
+++ src/predict.py
@@ -3,13 +3,15 @@ import numpy as np
 import pickle as pkl
 import sklearn
 
-def make_prediction(model, test_data, standardize):
+def make_prediction(model, test_data, standardize, save_to_file=None):
     """Test your model: make predictions and calculate metrics.
     
     Args:
         model (str): directory of the model
-        val_data (str): directory of the preprocessed test set
+        test_data (str): directory of the preprocessed test set
         standardize (bool): if True, the features will be standardized with StandardScaler
+        save_to_file (str): path to save the results in .csv format; 
+                            if None (default) the resulting array wil not be saved
     """
     df = pd.read_csv(test_data, sep=";")
 
@@ -27,5 +29,9 @@ def make_prediction(model, test_data, standardize):
 
     print("Accuracy on test set is: ", np.round(sklearn.metrics.accuracy_score(target, predictions), 3))
     print("F1 on test set is: ", np.round(sklearn.metrics.f1_score(target, predictions), 3))
+
+    if save_to_file is not None:
+        df["Predicted"] = predictions
+        df.to_csv(save_to_file, sep=";")
 
 
